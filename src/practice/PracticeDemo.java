5389c7f9f33087eef5bec562943efed042d4dd3f
sterling23-madrid <120076006+sterling23-madrid@users.noreply.github.com>
2023-07-29 09:56:32 -0600

    Update PracticeDemo.java

--- src/practice/PracticeDemo.java
+++ src/practice/PracticeDemo.java
@@ -3,6 +3,9 @@ package practice;
 public class PracticeDemo {
 	public static void main(String[] args) {
 		System.out.println("Sterling Madrid");
+
+		String city = "MD";
+		String state = "USA"
 	}
 
 }
